833a15f79fc42e22374139c958ca541528d295cc
Aryaman082-stack <deepamishra0815@gmail.com>
2025-06-29 12:22:52 +0530

    Add files via upload

---
+++ Piccolo.py
@@ -0,0 +1,3 @@
+a= input("enter your first name: ")
+b= input("enter your last name: ")
+print("hello , ARYAMAN MISHRA! welcome to the python program")
---
+++ kakarot.py
@@ -0,0 +1,8 @@
+num1=input('num1: 3')
+num2=input('num2: 12')
+print("addition:15 ")
+print("subtraction:9 ")
+print("multiplication:36 ")
+print("division:4.0 ")
+
+
